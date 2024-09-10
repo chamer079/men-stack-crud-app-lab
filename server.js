ecc1186f19c036856c577f9515e1bc5b68dbc9dc
chamer79 <chamer79@gmail.com>
2024-09-10 16:54:22 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -2,17 +2,32 @@
 //         IMPORTS
 //***************************
 const express = require("express")
-
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
 
 
 //***************************
 //   APP & CONFIGURATIONS
 //***************************
+dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
+
+mongoose.connect(process.env.MONGODB_URI) //<- opens connection to MongoDB
+
+// mongoose event listener
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+// mongoose event listener
+mongoose.connection.on("error", (err) => {
+    console.log(err)
+})
 
 
 
